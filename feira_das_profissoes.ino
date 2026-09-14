9a082618faa7f0693eb37ab3243657f065a66c27
Antonio-HOS <ahosantos.cic@uesc.br>
2026-09-13 23:35:36 -0300

    first commit

---
+++ feira_das_profissoes.ino
@@ -0,0 +1,157 @@
+//=================> Hardware
+//================= ENA-    --> GND Arduino
+//================= ENA+    --> Pino 8 Arduino
+//================= DIR-    --> GND Arduino
+//================= DIR+    --> Pino 2 Arduino
+//================= PUL-    --> GND Arduino
+//================= PUL+    --> Pino 5 Arduino
+//================= PUL-    --> GND Arduino
+//================= GND     --> GND da Fonte Externa
+//================= VCC     --> Fonte Externa
+
+/*
+Graus validos:
+                0.225
+                0.45
+                0.9                         
+                1.125                       
+                1.8                         
+                2.25                        
+                3.6                         
+                4.5                          
+                5.625                       
+                7.2                         
+                9                           
+                11.25                      
+                14.4                        
+                18
+                22.5
+                36
+                45
+                72
+                70
+                180
+*/
+
+//=================> Declarações
+const int stepPin = 5;
+const int dirPin = 2;
+const int enPin = 8;
+const int ledPin = 3;  // Define o pino que controla o relé (a fonte)
+
+const int stepsPerRevolution = 1600;
+const float grausPorRevolucao = 360.0;
+float grausAcumulados = 0.0;
+const int delayPasso = 1000;
+
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(enPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(enPin, LOW);
+  digitalWrite(ledPin, HIGH);
+
+  Serial.begin(9600);
+  Serial.println("Sistema pronto. Envie 1 para girar, 2 para disparar a fonte, 3 para girar uma volta completa, 4 para reiniciar o contador.");
+}
+
+void girarGraus(float graus) {
+  if (graus <= 0 || (grausAcumulados + graus) > grausPorRevolucao) {
+    Serial.println("Movimento inválido, excede 360° ou é zero.");
+    return;
+  }
+
+  int passos = (int)((graus / grausPorRevolucao) * stepsPerRevolution);
+
+  Serial.print("Girando ");
+  Serial.print(graus, 2);
+  Serial.println(" graus.");
+
+  digitalWrite(dirPin, HIGH);
+  for (int x = 0; x < passos; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(delayPasso);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(delayPasso);
+  }
+
+  grausAcumulados += graus;
+  Serial.print("Total girado: ");
+  Serial.print(grausAcumulados, 2);
+  Serial.println(" graus.");
+
+  if (abs(grausAcumulados - 360.0) < 0.01) {
+    grausAcumulados = 0.0;
+    Serial.println("Total de Graus igual a 360, contador de graus zerado!!");
+  }
+}
+
+void girar20Vezes() {
+  grausAcumulados = 0.0;
+
+  Serial.println("Iniciando sequência de 20 posições de 18 graus.");
+
+  for (int i = 0; i < 20; i++) {
+    Serial.print("Posição ");
+    Serial.print(i + 1);
+    Serial.println(" de 20");
+
+    girarGraus(18.0);
+
+    // Espera 2 segundos antes do próximo movimento
+    delay(2000);
+  }
+
+  grausAcumulados = 0.0;
+
+  Serial.println("Sequência concluída: 20 posições / 360 graus.");
+}
+
+void disparaFonte() {
+  digitalWrite(ledPin, LOW);  // Ativa o relé (nível lógico baixo)
+  Serial.println("Fonte disparada (relé ligado).");
+  delay(5000);                 // Mantém ligado por 5 segundos
+  digitalWrite(ledPin, HIGH);  // Desativa o relé
+  Serial.println("Fonte desligada (relé desligado).");
+}
+
+void girandoVolta() {
+  grausAcumulados = 0.0;
+  for (int i = 0; i < 200; i++)
+    girarGraus(1.8);
+}
+
+void reinicarContador() {
+  grausAcumulados = 0.0;
+  Serial.println("Contador de graus zerado!!");
+  Serial.print("Total girado: ");
+  Serial.print(grausAcumulados, 2);
+  Serial.println(" graus.");
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    String comando = Serial.readStringUntil('\n');
+    comando.trim();
+
+    if (comando == "1") {
+      while (Serial.available() == 0)
+        ;  // espera valor dos graus
+      float graus = Serial.readStringUntil('\n').toFloat();
+      girarGraus(graus);
+    } else if (comando == "2") {
+      disparaFonte();
+    } else if (comando == "3") {
+      girandoVolta();
+    } else if (comando == "4") {
+      reinicarContador();
+    } else if (comando == "5") {
+      girar20Vezes();
+    } else {
+      Serial.println("Comando inválido.");
+    }
+  }
+}
